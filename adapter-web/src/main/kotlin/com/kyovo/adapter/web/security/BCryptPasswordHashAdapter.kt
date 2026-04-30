fb59e0057b4bcf4f4cc7e333d9e3c447380897ea
Koimi Yovo <yovoedem33@gmail.com>
2026-05-01 00:13:22 +0200

    dependency injection for BCryptPasswordHashAdapter

--- adapter-web/src/main/kotlin/com/kyovo/adapter/web/security/BCryptPasswordHashAdapter.kt
+++ adapter-web/src/main/kotlin/com/kyovo/adapter/web/security/BCryptPasswordHashAdapter.kt
@@ -2,14 +2,12 @@ package com.kyovo.adapter.web.security
 
 import com.kyovo.domain.model.UserPassword
 import com.kyovo.domain.port.secondary.PasswordHashPort
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
-class BCryptPasswordHashAdapter : PasswordHashPort
+class BCryptPasswordHashAdapter(private val encoder: PasswordEncoder) : PasswordHashPort
 {
-    private val encoder = BCryptPasswordEncoder()
-
     override fun hash(raw: String): UserPassword
     {
         return UserPassword(encoder.encode(raw))
--- adapter-web/src/main/kotlin/com/kyovo/adapter/web/security/SecurityConfig.kt
+++ adapter-web/src/main/kotlin/com/kyovo/adapter/web/security/SecurityConfig.kt
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -38,5 +40,11 @@ class SecurityConfig(private val jwtAuthenticationFilter: JwtAuthenticationFilte
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder
+    {
+        return BCryptPasswordEncoder()
     }
 }
