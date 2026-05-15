2d0e71517db502006aa4a336ad363b9a13246314
Koimi Yovo <yovoedem33@gmail.com>
2026-05-15 19:50:44 +0200

    fix tests

--- bootstrap/src/test/kotlin/com/kyovo/AbstractIntegrationTest.kt
+++ bootstrap/src/test/kotlin/com/kyovo/AbstractIntegrationTest.kt
@@ -4,14 +4,19 @@ import com.kyovo.config.TestTimeProviderConfig
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.context.annotation.Import
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(TestTimeProviderConfig::class)
 abstract class AbstractIntegrationTest
 {
+    @MockitoBean
+    lateinit var mailSender: JavaMailSender
+
     companion object
     {
         @JvmStatic
